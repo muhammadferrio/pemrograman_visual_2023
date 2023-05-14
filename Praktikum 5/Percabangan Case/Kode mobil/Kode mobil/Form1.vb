5be8350ce4a3a2c9df3badd7624e34138daae7f6
muhammadferrio <103301814+muhammadferrio@users.noreply.github.com>
2023-05-14 17:19:52 +0700

    Add files via upload

---
+++ Praktikum 5/Percabangan Case/Kode mobil/Kode mobil/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Select Case txtKodeMobil.Text
+            Case "INV" : txtKeterangan.Text = "Inova"
+
+            Case "XNA" : txtKeterangan.Text = "Xenia"
+
+            Case "PJR" : txtKeterangan.Text = "Pajero"
+
+            Case Else : txtKeterangan.Text = "Kode Mobil tidak ditemukan"
+        End Select
+    End Sub
+End Class
